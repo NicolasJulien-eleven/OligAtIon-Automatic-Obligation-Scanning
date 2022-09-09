3c0514b4ff0dca4365da3f84e73fbf68d012f536
NicolasJulienMazars <113121339+NicolasJulienMazars@users.noreply.github.com>
2022-09-09 11:32:58 +0200

    Update ObligAtIon_Automatic_Document_Scanning.py

--- ObligAtIon_Automatic_Document_Scanning.py
+++ ObligAtIon_Automatic_Document_Scanning.py
@@ -70,7 +70,7 @@ if st.session_state.document_upload:
 with st.spinner("Chargement du modèle..."):
     
     pytesseract.pytesseract.tesseract_cmd = r'C:\Users\nicolas.julien\AppData\Local\Tesseract-OCR\tesseract.exe'
-    path_model = r'C:\Users\nicolas.julien\Documents\Deployment\checkpoint-best'
+    path_model = r'NicolasJulienData/ObligAtIon'
 
     
     #Definition des fonctions utiles------------------------------------------------------------------------------------------------------------------------------------------------
